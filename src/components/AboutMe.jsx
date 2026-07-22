2dfc35fe4e4b8e7ac3081f2f08894b6cad07af72
Hanre Delport <hanredelport@gmail.com>
2026-07-22 20:36:29 +0200

    Changed About me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,8 @@
 import ExpandableCardRow from './ExpandableCardRow'
 import TagList from './TagList'
 
+import rolesIcon from '../assets/icons/role.png'
 import interestsIcon from '../assets/icons/interests.png'
-import hobbiesIcon from '../assets/icons/hobbies.png'
 import dreamsIcon from '../assets/icons/dreams.png'
 import goalsIcon from '../assets/icons/goals.png'
 import aspirationsIcon from '../assets/icons/aspirations.png'
@@ -15,19 +15,20 @@ const aboutMeItems = [
     content: <TagList tags={['Theatre Plays', 'Golf', 'Gym', 'Padel', 'Gaming', 'Programming']} color="purple" />,
   },
   {
-    title: 'Dreams',
-    icon: dreamsIcon,
-    content: <TagList tags={['Tech Start-up CEO', 'Porsche 911 GT3 RS', 'Sea-view apartment']} color="purple" />,
-  },
-  {
-    title: 'Goals',
+    title: 'Career Aspirations',
     icon: goalsIcon,
-    content: <TagList tags={['Acquire honours and MBA', ' Complete first project as a project manager', 'Buy my first company']} color="purple" />,
+    content: <TagList tags={['Join a leading multinational technology company','Lead multidisciplinary software teams', 'Become a Technical Project Manager',
+  'Build scalable software systems for corporate clients', 'Complete Honours and an MBA',
+  'Launch a successful technology company']} color="purple" />,
   },
   {
-    title: 'Aspirations',
-    icon: aspirationsIcon,
-    content: <TagList tags={['Escape Comfort Zone', 'Raise a Family', 'Leave Lasting Legacy']} color="purple" />,
+  title: 'Desired Roles',
+  icon: rolesIcon,
+  content: 
+    <TagList
+      tags={[
+        'Software Engineer', 'Full-Stack Developer', 'Technical Project Manager', 'Solutions Architect',
+        'IT Consultant', 'Intelligent Systems Engineer']} color="purple"/>, 
   },
   {
     title: 'Dislikes',
